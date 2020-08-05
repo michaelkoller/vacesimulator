7ce314a40d2f90a7fba1e22c33978174990d3bc7
michaelkoller <koller_michael@gmx.net>
2020-08-05 17:55:06 +0200

    Record Object States For Later Rendering 1
    Positions and rotations are stored in a txt file per frame per object with renderer. Next step: restore frame from these txt files for slow rendering of video.

---
+++ V4R-VR-Kitchen-v6/Assets/PlayModeManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+
+
+public class PlaybackState
+{   
+    public string path;
+    public int frameNumber;
+    public float time;
+    public float deltaTime;
+    private System.IO.StreamReader reader;
+    private int frameCounter;
+    
+    private Renderer[] allRenderers;
+    private GameObject[] allGameObjectsWithRenderer;
+    private Dictionary<string, GameObject> gameObjectDict = new Dictionary<string, GameObject>();
+    public PlaybackState(string path)
+    {
+        allRenderers = GameObject.FindObjectsOfType<Renderer>();
+        allGameObjectsWithRenderer = new GameObject[allRenderers.Length];
+        for (int i = 0; i < allRenderers.Length; i++)
+        {
+            allGameObjectsWithRenderer[i] = allRenderers[i].gameObject;
+            gameObjectDict.Add(allRenderers[i].gameObject.name, allRenderers[i].gameObject);
+        }        
+        this.path = path;
+        this.reader = new StreamReader(this.path);
+        this.frameCounter = 0;
+        
+    }
+
+    public void GetStateOfFrame()
+    {
+        this.frameNumber = int.Parse(reader.ReadLine().Trim().Split(' ')[1]);
+        this.time = int.Parse(reader.ReadLine().Trim());
+        this.deltaTime = int.Parse(reader.ReadLine().Trim());
+        for (int i = 0; i < allGameObjectsWithRenderer.Length; i++)
+        {
+            string name = reader.ReadLine().Trim().Split((' '))[0];
+            string[] pos = reader.ReadLine().Trim('(').Trim(')').Split(',');
+            string[] rot = reader.ReadLine().Trim('(').Trim(')').Split(',');
+            float[] posFloat = new float[3];
+            float[] rotFloat = new float[4];
+            for (int j = 0; j < 3; j++)
+            {
+                posFloat[j] = float.Parse(pos[j]);
+            }
+            for (int j = 0; j < 3; j++)
+            {
+                rotFloat[j] = float.Parse(rot[j]);
+            }
+            this.gameObjectDict[name].transform.position = new Vector3(posFloat[0],posFloat[1],posFloat[2]);
+            this.gameObjectDict[name].transform.rotation = new Quaternion(rotFloat[0],rotFloat[1],rotFloat[2], rotFloat[3]);
+            
+        }
+    }
+}
+
+public class PlayModeManager : MonoBehaviour
+{
+    private string directory = "Assets/RecordingsForRender/";
+    private DirectoryInfo di;
+    private FileInfo[] fileInfos;
+    public bool playback = false;
+
+    private PlaybackState ps;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!playback)
+        {
+            //Leave everything as it is in the project settings
+        }
+        else
+        {
+            //Adjust physics settings to prepare for playback mode
+            Physics.autoSimulation = false;
+            di = new DirectoryInfo(directory);
+            fileInfos = di.GetFiles();
+            Debug.Log("FITEST");
+            foreach (var fileInfo in fileInfos)
+            {
+                Debug.Log(fileInfo);
+            }
+            Debug.Log("FILE NAME "+fileInfos[0].FullName);
+            ps = new PlaybackState(fileInfos[0].FullName);
+            Debug.Log(ps);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ps.GetStateOfFrame();
+    }
+}
---
+++ V4R-VR-Kitchen-v6/Assets/Scripts/RecordObjectPosRot.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+//https://support.unity3d.com/hc/en-us/articles/115000341143-How-do-I-read-and-write-data-from-a-text-file-
+
+public class RecordObjectPosRot : MonoBehaviour
+{
+    int currentFrame = 0;
+    private string path;
+    private GameObject[] allGameObjects;
+    private Renderer[] allRenderers;
+    private GameObject[] allGameObjectsWithRenderer;
+    private StringBuilder sb;
+
+    string GetPath()
+    {
+        return "Assets/RecordingsForRender/test"+ currentFrame.ToString() +".txt";
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    { 
+        allGameObjects = GameObject.FindObjectsOfType<GameObject>();
+        allRenderers = FindObjectsOfType<Renderer>();
+        allGameObjectsWithRenderer = new GameObject[allRenderers.Length];
+        for (int i = 0; i < allRenderers.Length; i++)
+        {
+            allGameObjectsWithRenderer[i] = allRenderers[i].gameObject;
+        }
+        path = GetPath();
+        //Debug.Log(allGameObjects.Length);
+        sb = new StringBuilder();
+        Debug.Log("ALL RENDERERS LENGTH " + allGameObjectsWithRenderer.Length.ToString());
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        sb.AppendLine("frame " + currentFrame.ToString());
+        sb.AppendLine(Time.time.ToString());
+        sb.AppendLine(Time.deltaTime.ToString());
+        for (int i = 0; i < allGameObjectsWithRenderer.Length; i++)
+        {
+            sb.AppendLine(allGameObjectsWithRenderer[i].name);
+            sb.AppendLine(allGameObjectsWithRenderer[i].transform.position.ToString("F3"));
+            sb.AppendLine(allGameObjectsWithRenderer[i].transform.rotation.ToString("F3"));
+        }
+
+        if (currentFrame % 100 == 0)
+        {
+            File.WriteAllText(path, sb.ToString());
+            sb.Clear();
+        }
+        currentFrame++;
+        path = GetPath();
+    }
+}
