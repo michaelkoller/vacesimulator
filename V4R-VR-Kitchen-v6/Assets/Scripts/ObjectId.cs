73ae00a62868c9ab9ac16533b03c03b9f057d502
michaelkoller <koller_michael@gmx.net>
2020-09-18 17:50:29 +0200

    On Predicate 1

--- V4R-VR-Kitchen-v6/Assets/Scripts/ObjectId.cs
+++ V4R-VR-Kitchen-v6/Assets/Scripts/ObjectId.cs
@@ -78,6 +78,8 @@ public class ObjectId : MonoBehaviour
             mc.convex = true;
             mc.isTrigger = true;
         }
+
+        this.gameObject.AddComponent<OnPredicate>();
     }
 
     private void MakeGhost()
