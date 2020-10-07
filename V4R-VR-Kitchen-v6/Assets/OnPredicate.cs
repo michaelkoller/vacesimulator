8dc1521f9ba4fcfa32424a5b6b5540da7bad0be7
michaelkoller <koller_michael@gmx.net>
2020-10-07 18:06:52 +0200

    scene cleaned

--- V4R-VR-Kitchen-v6/Assets/OnPredicate.cs
+++ V4R-VR-Kitchen-v6/Assets/OnPredicate.cs
@@ -38,57 +38,7 @@ public class OnPredicate : MonoBehaviour
         thisObjectId = GetComponent<ObjectId>();
         recordObjectPosRot = GameObject.FindGameObjectWithTag("Manager").GetComponent<RecordObjectPosRot>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // foreach(Vector3 v in rayCastDirections){
-        //     RaycastHit hit;
-        //     // Does the ray intersect any objects excluding the player layer
-        //     if (Physics.Raycast(transform.position, v, out hit, 100))
-        //     {
-        //         Debug.DrawRay(transform.position, v * hit.distance, Color.yellow);
-        //         Debug.Log(v + " did Hit " + hit.collider.gameObject.GetComponent<ObjectId>().objectName);
-        //     }
-        //     else
-        //     {
-        //         Debug.DrawRay(transform.position, v * 1000, Color.white);
-        //     }
-        // }
-    }
-
-    // private void OnCollisionEnter(Collision collision)
-    // {
-    //     results = new NativeArray<RaycastHit>(rayCastDirections.Length, Allocator.TempJob);
-    //     commands = new NativeArray<RaycastCommand>(rayCastDirections.Length, Allocator.TempJob);
-    //     GameObject otherGo = collision.gameObject;
-    //     ObjectId otherObjectId = otherGo.GetComponentInParent<ObjectId>();
-    //     for(int i = 0; i < rayCastDirections.Length; i++)
-    //     {
-    //         commands[0] = new RaycastCommand(transform.position, rayCastDirections[i], 100, maxHits:1);
-    //     }
-    //     var handle = RaycastCommand.ScheduleBatch(commands, results, 1);
-    //     handle.Complete();
-    //
-    //     for (int i = 0; i < results.Length; i++)
-    //     {
-    //         RaycastHit hit = results[i];
-    //         if (hit.transform != null)
-    //         {
-    //             if (otherObjectId != null && otherObjectId == hit.transform.gameObject.GetComponentInParent<ObjectId>() && !currentlyOnList.Contains(otherObjectId))
-    //             {
-    //                 currentlyOnList.Add(otherObjectId);
-    //                 Debug.Log(thisObjectId.objectName + " is on " + otherObjectId.objectName);
-    //                 break;
-    //             }
-    //         }
-    //     }
-    //     
-    //     results.Dispose();
-    //     commands.Dispose();
-    // }
-
-    //Single Raycast:
+    
     void OnCollisionEnter(Collision collision)
     {
         GameObject otherGo = collision.gameObject;
@@ -104,6 +54,10 @@ public class OnPredicate : MonoBehaviour
                 {
                     currentlyOnList.Add(otherObjectId);
                     string n = thisObjectId != null? thisObjectId. objectName : this.gameObject.name;
+                    if (recordObjectPosRot == null)
+                    {
+                        recordObjectPosRot = GameObject.FindGameObjectWithTag("Manager").GetComponent<RecordObjectPosRot>();
+                    }
                     recordObjectPosRot.RecordOnPredicate(n,otherObjectId.objectName, "start_touching");
                     //Debug.Log(n + " is on " + otherObjectId.objectName);
                     break;
